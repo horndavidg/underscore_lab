b5da41c8388fbb5387f4a463a1e6c587baea540c
David Horn <horndavidg@gmail.com>
2015-05-04 23:03:38 -0700

    Updated for Part 2

--- underscore.js
+++ underscore.js
@@ -112,7 +112,155 @@ var _ = (function() {
 
 console.log(_.shuffle([1,11,3,4,5,6,7]));
 
+// _.sample: Produce a random sample from the list. 
+//Pass a number to return n random elements from the list. 
+//Otherwise a single random item will be returned.
+
+  
+var _ = (function() {
+  return {
+  sample: function(arr, num) {
+  var rand;
+  if(num){
+    var result = [];
+    for(var i = 0; i < num; i++){
+       rand = Math.floor(Math.random() * arr.length);
+       result.push(arr[rand]);
+    }
+    console.log(result);
+  } else {
+      rand = Math.floor(Math.random() * arr.length);
+      console.log(arr[rand]);
+         }
+       }
+    };
+})();
+
+var nums = [1,2,3,4,5,6,7,8,9];
+
+_.sample(nums, 8);
+
+// _.difference: returns the values from array that 
+//are not present in the other array.
+
+var _ = (function() {
+  return {
+    difference: function(a1, a2){
+      var a = [];
+     var diff = [];
+      for (var i = 0; i < a1.length; i++) 
+       a = a1[i];
+      for (var j = 0; j < a2.length; j++)
+        if (a !== a2[j])
+          diff.push(a2[j]);
+          console.log(diff);
+        }
+     };
+})();
+
+_.difference([1, 2, 3, 4, 5], [5, 2, 10]);
 
 
+// _.indexOf: Returns the index at which value 
+//can be found in the array, or -1 if value is not 
+//present in the array.
 
+var _ =(function() {
+        return {
+        indexOf: function(inputArray, inputValue) {
+        for (var i = 0; i <= inputArray.length; i++){
+        if(inputArray[i] === inputValue) {
+             console.log(i);
+                  }
+               }
+             }
+        };
+})();
+
+
+_.indexOf([1, 2, 3], 2);
+
+// _.pluck: extracts a list of property values and returns 
+//them in an array.
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+
+var _ =(function() {
+  return {
+ pluck: function(list, inputkey) {
+  var arr = [];
+  for (var i = 0; i < list.length; i++){
+   arr.push(list[i][inputkey]); 
+            }
+  return arr;
+             }
+        };
+ })();
+
+console.log(_.pluck(stooges, 'name'));
+
+// _.each: iterates over an array and calls a given function 
+//with each element.
+
+var _ = (function() { 
+  return {
+  each: function(array, action) {
+  for (var i = 0; i < array.length; i++)
+    action(array[i]);
+           }
+       };
+ })();
+
+_.each([1, 2, 3], alert);
+
+// _.compact: returns a new array with all provided undefined 
+//values removed. 
+
+var _ = (function() {
+    return {  
+    compact: function(array) {
+    var answer = [];
+    for (var i = 0; i < array.length; i++)
+    if (array[i] !== undefined) {
+      answer.push(array[i]);
+    }
+    console.log(answer);
+              }
+           };
+    })();
+
+_.compact([1, "hello", undefined, 3, undefined, 8]);
+
+// _.map: returns a new array of values produced by running each 
+//element of an array through a given function.
+
+var _ = (function () {
+   return {
+     map: function(array, action) {
+          var answer = [];
+          for (var i = 0; i < array.length; i++)
+          answer.push(action(array[i]));
+          console.log(answer);
+                 }
+               };
+           })();
+  
+_.map([1, 2, 3], function(num){ return num * 3; });
+
+// _.filter: Looks through each value in the list, returning 
+//an array of all the values that pass a truth test.
+
+var _ = (function () {
+   return {
+     filter: function(array, action) {
+          var answer = [];
+          for (var i = 0; i < array.length; i++)
+          if (action(array[i]) === true) 
+          answer.push(array[i]);
+          console.log(answer);
+              }
+          };
+    })();
+  
+_.filter([1, 2, 3, 4, 5, 6, 8], function(num){ return num % 2 === 0; });
 
